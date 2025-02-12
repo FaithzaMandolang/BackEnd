a998486155fa44cf8164b59ba976d5812ec7a348
FaithzaMandolang <s22210337@student.unklab.ac.id>
2025-02-12 09:50:19 +0800

    push exercise 3

--- index.js
+++ index.js
@@ -1,46 +1,26 @@
 const http = require("https");
-const { hello, greetings } = require("./helloWorld");
+const users = require("./users");
 const moment = require("moment");
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("this is homepage"));
+
+app.get("/users", (req, res) =>
+  res.status(200).json({
+    users,
+  })
+);
+
 app.get("/about", (req, res) =>
   res.status(200).json({
     status: " success",
     message: "About page",
-    data: [],
+    message: "response success",
+    Description: "Exercise #03",
+    date: moment().format(),
   })
 );
-
-app.post("/contoh", (req, res) => {
-  res.send("Request dengan method post");
-});
-
-app.put("/contoh", (req, res) => {
-  res.send("Request dengan method put");
-});
-
-app.delete("/contoh", (req, res) => {
-  res.send("Request dengan method delete");
-});
-
-app.patch("/contoh", (req, res) => {
-  res.send("Request dengan method patch");
-});
-
-app.all("/universal", (req, res) => {
-  res.send(`Request dengan method ${req.method}`);
-});
-
-app.get("/news/:id", (req, res) => {
-  res.send(`Artikel ke- ${req.params.id}`); //routing dinamis pake param/parameter
-});
-
-app.get("/foods", (req, res) => {
-  const { page, sort } = req.query;
-  res.send(`query yang didapat adalah ${page}, sort by ${sort}`); //routing menggunakan query
-});
 
 const hostname = "127.0.0.1";
 const port = 3000;
