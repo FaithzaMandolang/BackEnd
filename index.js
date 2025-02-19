cd876399f04d0912bfd6752e65a7a7f2af7708eb
FaithzaMandolang <s22210337@student.unklab.ac.id>
2025-02-19 14:12:11 +0800

    exe 4

--- index.js
+++ index.js
@@ -3,43 +3,34 @@ const users = require("./users");
 const moment = require("moment");
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
-const errorhandler = require("errorhandler");
+// const morgan = require("morgan");
+// const errorhandler = require("errorhandler");
 
-//middleware ditulis biasanya di atas routing (request)
-// const log = (req, res, next) => {
-//   console.log(
-//     moment().format("MMMM Do YYYY, h:mm:ss a") +
-//       " " +
-//       req.originalUrl +
-//       " " +
-//       req.ip
-//   );
-
-//   next();
-// };
-
-app.use(morgan("tiny"));
-app.use(errorhandler);
+// app.use(morgan("tiny"));
 
 //routing
 app.get("/", (req, res) => res.send("this is homepage"));
 
+//get user seluruh data
 app.get("/users", (req, res) =>
   res.status(200).json({
     users,
   })
 );
 
-app.get("/about", (req, res) =>
-  res.status(200).json({
-    status: " success",
-    message: "About page",
-    message: "response success",
-    Description: "Exercise #03",
-    date: moment().format(),
-  })
-);
+//get user berdasarkan nama
+app.get("/users/:name", (req, res) => {
+  const name = req.params.name.toLowerCase();
+  const user = users.find((data) => data.name.toLowerCase() === name);
+
+  if (!user) {
+    return res.status(404).json({
+      message: "Data user tidak ditemukan",
+    });
+  }
+
+  res.status(200).json({ user });
+});
 
 //middleware respon/biasa untuk 404
 app.use((req, res, next) => {
@@ -48,6 +39,16 @@ app.use((req, res, next) => {
     message: "resource tidak ditemukan",
   });
 });
+
+//penanganan error
+const errorhandling = (err, req, res, next) => {
+  res.json({
+    status: "error",
+    message: "terjadi kesalahan pada server",
+  });
+};
+
+app.use(errorhandling);
 
 const hostname = "127.0.0.1";
 const port = 3000;
