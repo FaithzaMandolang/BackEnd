b2933be78692cfd5a2d0a7ddc87ddbe2fa4a0746
FaithzaMandolang <s22210337@student.unklab.ac.id>
2025-04-14 08:56:25 +0800

    connect to mongodb

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const users = require("./users");
 const routers = express.Router();
 const path = require("path");
+const client = require("./mongodb");
 
 //Middleware untuk file upload
 const fs = require("fs");
@@ -54,83 +55,99 @@ routers.put("/login", (req, res) => {
 });
 
 //get user seluruh data
-routers.get("/users", (req, res) =>
-  res.status(200).json({
-    users,
-  })
-);
+// routers.get("/users", (req, res) =>
+//   res.status(200).json({
+//     users,
+//   })
+// );
+
+routers.get("/users", async (req, res) => {
+  try {
+    const db = client.db("test");
+    const users = await db.collection("users").find().toArray();
+    res.json({
+      status: "success",
+      message: "list users",
+      data: users,
+    });
+  } catch (error) {
+    res.json({
+      status: "error",
+    });
+  }
+});
 
 //get user berdasarkan nama
-routers.get("/users/:name", (req, res) => {
-  const name = req.params.name.toLowerCase();
-  const user = users.find((data) => data.name.toLowerCase() === name);
+// routers.get("/users/:name", (req, res) => {
+//   const name = req.params.name.toLowerCase();
+//   const user = users.find((data) => data.name.toLowerCase() === name);
 
-  if (!user) {
-    return res.status(404).json({
-      message: "Data user tidak ditemukan",
-    });
-  }
+//   if (!user) {
+//     return res.status(404).json({
+//       message: "Data user tidak ditemukan",
+//     });
+//   }
 
-  res.status(200).json({ user });
-});
+//   res.status(200).json({ user });
+// });
 
-routers.post("/users", (req, res) => {
-  const { name, id } = req.body;
+// routers.post("/users", (req, res) => {
+//   const { name, id } = req.body;
 
-  if (!name || !id) {
-    return res.status(400).json({ error: "Masukan data yang akan diubah" });
-  }
+//   if (!name || !id) {
+//     return res.status(400).json({ error: "Masukan data yang akan diubah" });
+//   }
 
-  const newUser = { id, name };
-  res.status(201).json({
-    message: "User successfully created",
-    user: newUser,
-  });
-});
+//   const newUser = { id, name };
+//   res.status(201).json({
+//     message: "User successfully created",
+//     user: newUser,
+//   });
+// });
 
-routers.put("/users/:name", (req, res) => {
-  const name = req.params.name.toLowerCase();
-  const { newName } = req.body;
+// routers.put("/users/:name", (req, res) => {
+//   const name = req.params.name.toLowerCase();
+//   const { newName } = req.body;
 
-  if (!newName) {
-    return res.status(400).json({
-      error: "Harus memasukkan setidaknya satu data untuk diperbarui",
-    });
-  }
+//   if (!newName) {
+//     return res.status(400).json({
+//       error: "Harus memasukkan setidaknya satu data untuk diperbarui",
+//     });
+//   }
 
-  // cari user
-  const userIndex = users.findIndex((data) => data.name.toLowerCase() === name);
+//   // cari user
+//   const userIndex = users.findIndex((data) => data.name.toLowerCase() === name);
 
-  // user tidak ditemukan
-  if (userIndex === -1) {
-    return res.status(404).json({ error: "Data user tidak ditemukan" });
-  }
+//   // user tidak ditemukan
+//   if (userIndex === -1) {
+//     return res.status(404).json({ error: "Data user tidak ditemukan" });
+//   }
 
-  // edit data user
-  if (newName) users[userIndex].name = newName;
+//   // edit data user
+//   if (newName) users[userIndex].name = newName;
 
-  res.status(200).json({
-    message: "User berhasil diperbarui",
-    user: users[userIndex],
-  });
-});
+//   res.status(200).json({
+//     message: "User berhasil diperbarui",
+//     user: users[userIndex],
+//   });
+// });
 
-routers.delete("/users/:name", (req, res) => {
-  const name = req.params.name.toLowerCase();
+// routers.delete("/users/:name", (req, res) => {
+//   const name = req.params.name.toLowerCase();
 
-  const userIndex = users.findIndex((user) => user.name.toLowerCase() === name);
+//   const userIndex = users.findIndex((user) => user.name.toLowerCase() === name);
 
-  if (userIndex === -1) {
-    return res.status(404).json({ error: "User not found" });
-  }
+//   if (userIndex === -1) {
+//     return res.status(404).json({ error: "User not found" });
+//   }
 
-  // Hapus user dari array
-  const deletedUser = users.splice(userIndex, 1);
+//   // Hapus user dari array
+//   const deletedUser = users.splice(userIndex, 1);
 
-  res.status(200).json({
-    message: "User successfully deleted",
-    deletedUser: deletedUser[0],
-  });
-});
+//   res.status(200).json({
+//     message: "User successfully deleted",
+//     deletedUser: deletedUser[0],
+//   });
+//});
 
 module.exports = routers;
